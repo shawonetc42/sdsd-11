dcba59a15095e831fb2b52c81f2522f7c5a4118c
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-10-07 15:14:13 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,25 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
+import Posts from "./Server/Posts";
+import Loading from "./home/loading";
 
-export default async function Home() {
-  // Fetching data from JSONPlaceholder API
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .catch((error) => console.error("Error fetching data:", error));
-
+export default function PostsPage() {
   return (
-    <div className="bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold text-blue-500 mb-4">
-        Welcome to Next.js with Tailwind CSS!
-      </h1>
-      {data.map((post) => (
-        <div key={post.id} className="bg-white p-4 rounded-lg shadow-md mb-4">
-          <h2 className="text-2xl font-bold text-blue-600">
-            <Link href={`/posts/${post.id}`}>{post.title}</Link>
-          </h2>
-          <p className="text-gray-700">{post.body}</p>
-        </div>
-      ))}
+    <div className="">
+      <React.Suspense fallback={<Loading />}>
+        <Posts />
+      </React.Suspense>
     </div>
   );
 }
