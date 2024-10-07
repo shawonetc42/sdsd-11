9ea75938e2128d24e8db1f4d8b3e5a120fdff31d
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-10-07 14:04:26 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,25 @@
-// app/page.js
-import React from "react";
+import Image from "next/image";
 import Link from "next/link";
-import TruncatedText from "@/components/helper/TruncatedText";
-import CommentActions from "@/components/model/Comments";
-import Writecomment from "@/components/model/Writecomment";
-import timeSince from "@/components/helper/timeSince";
-import NavigationMenu from "@/components/model/navigationMenu";
-import RightSide from "@/components/model/rightSide";
-import PostList from "./pagi/page";
-import StatusUpdate from "@/components/client/router/StatusUpdate";
-import PostsPage from "./b/page";
 
 export default async function Home() {
+  // Fetching data from JSONPlaceholder API
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .catch((error) => console.error("Error fetching data:", error));
+
   return (
-    <>
-      <main>
-        <h1>Hello World 3</h1>
-      </main>
-    </>
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-blue-500 mb-4">
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+      {data.map((post) => (
+        <div key={post.id} className="bg-white p-4 rounded-lg shadow-md mb-4">
+          <h2 className="text-2xl font-bold text-blue-600">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </h2>
+          <p className="text-gray-700">{post.body}</p>
+        </div>
+      ))}
+    </div>
   );
 }
