01128fd66bcdfcdc433c70c5d5bfe7c97f2d9a82
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-10-07 15:26:07 +0600

    Update page.js

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,18 +1,17 @@
+import TruncatedText from "@/components/helper/TruncatedText";
+
 // app/posts/[id]/page.js
 export default async function PostPage({ params }) {
   const { id } = params; // Get the post ID from the URL parameters
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const res = await fetch(`https://flashmain.vercel.app/posts/${id}`);
   const post = await res.json();
 
-  // Check if the post exists
-  if (!post.id) {
-    return <h1>Post not found</h1>;
-  }
-
   return (
-    <div className="bg-gray-100 p-4">
+    <div className="bg-gray-100 p-4 mt-20">
+      <h1> hello world</h1>
+      {/* <TruncatedText text={data.questiontext} />
       <h1 className="text-4xl font-bold text-blue-500">{post.title}</h1>
-      <p className="text-gray-700 mt-4">{post.body}</p>
+      <p className="text-gray-700 mt-4">{post.body}</p> */}
     </div>
   );
 }
